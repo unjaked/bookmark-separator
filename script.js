ff2e2151c70d7af06102d6149ec79d83ed70a7fc
Jake <100233532+unjaked@users.noreply.github.com>
2026-07-05 20:49:41 -0400

    Add favicon color picker

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 
 document.addEventListener("DOMContentLoaded", function () { // wait till  HTML page is fully loaded
-    console.log("DOM content loaded.");
 
     // Change favicon
     const favicon = document.getElementById("favicon");
-    const faviconButtons = document.querySelectorAll(".favicon-option");
+    const faviconColorInput = document.getElementById("faviconColor");
     
-    faviconButtons.forEach(function (button) {
-      button.addEventListener("click", function () {
-        const selectedFavicon = button.dataset.favicon;
-    
-        favicon.href = selectedFavicon
-            //   + "?v=" + generateHash(); // Makes browser reload icon each time instead of caching
-      });
+    Coloris({
+      el: "#faviconColor",
+      theme: "large",
+      // themeMode: 'auto',
+      alpha: true,
+      format: "rgb",
+      clearButton: false,
+      swatches: [
+        "rgba(0, 0, 0, 1)",
+        "rgba(94, 94, 94, 1)",
+        "rgba(255, 255, 255, 1)",
+        "rgba(178, 255, 229, 1)"
+      ]
     });
+    
+    function updateFavicon(color) {
+      const svg = `
+        <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <line
+            x1="8"
+            y1="1"
+            x2="8"
+            y2="16"
+            stroke="${color}"
+            stroke-width="2.0"
+          />
+        </svg>
+      `;
+    
+      favicon.href = "data:image/svg+xml," + encodeURIComponent(svg);
+    }
+    
+    faviconColorInput.addEventListener("input", function () {
+      updateFavicon(faviconColorInput.value);
+    });
+    
+    updateFavicon(faviconColorInput.value);
+
 
     // Hash generation
     function generateHash() {
@@ -21,6 +50,6 @@ document.addEventListener("DOMContentLoaded", function () { // wait till  HTML p
         console.log(hash);
         return hash + Date.now(); // Append URL with # and the hash value + current UNIX time
     }
-    
+
     window.location.hash = generateHash();
 });
